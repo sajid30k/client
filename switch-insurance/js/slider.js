45ce5e19a7410e34f3a2bff9dfef95b35209e5a5
sajid30k <35804171+sajid30k@users.noreply.github.com>
2024-01-31 22:50:57 +0600

    Add files via upload

---
+++ switch-insurance/js/slider.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function($) {
+  var jssor_1_SlideshowTransitions = [
+    {
+      $Duration: 600,
+      x: -1,
+      $Delay: 12,
+      $Cols: 10,
+      $Rows: 5,
+      $SlideOut: true,
+      $Formation: $JssorSlideshowFormations$.$FormationZigZag,
+      $Assembly: 513,
+      $Easing: { $Left: $Jease$.$InCubic, $Opacity: $Jease$.$OutQuad },
+      $Opacity: 2
+    }
+  ];
+
+  var jssor_1_options = {
+    $AutoPlay: 1,
+    $Idle: 0,
+    $PauseOnHover: 0,
+    $SlideshowOptions: {
+      $Class: $JssorSlideshowRunner$,
+      $Transitions: jssor_1_SlideshowTransitions
+    },
+    $ArrowNavigatorOptions: {
+      $Class: $JssorArrowNavigator$
+    },
+    $BulletNavigatorOptions: {
+      $Class: $JssorBulletNavigator$,
+      $SpacingX: 20,
+      $SpacingY: 20
+    }
+  };
+
+  var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+  /*#region responsive code begin*/
+
+  var MAX_WIDTH = 3000;
+
+  function ScaleSlider() {
+    var containerElement = jssor_1_slider.$Elmt.parentNode;
+    var containerWidth = containerElement.clientWidth;
+
+    if (containerWidth) {
+      var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+      jssor_1_slider.$ScaleWidth(expectedWidth);
+    } else {
+      window.setTimeout(ScaleSlider, 30);
+    }
+  }
+
+  ScaleSlider();
+
+  $(window).bind("load", ScaleSlider);
+  $(window).bind("resize", ScaleSlider);
+  $(window).bind("orientationchange", ScaleSlider);
+  /*#endregion responsive code end*/
+});
